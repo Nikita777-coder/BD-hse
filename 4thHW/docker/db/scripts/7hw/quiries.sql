ef1e3db7ee2b6b90a9927e28c54550cc5fc630d3
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2023-12-19 16:52:01 +0300

    add 7thHw

--- 4thHW/docker/db/scripts/7hw/quiries.sql
+++ 4thHW/docker/db/scripts/7hw/quiries.sql
@@ -17,3 +17,33 @@ where e.is_team_event = 0 and r.result = r1.result and r.player_id != r1.player_
 and r.medal = 'Gold'
 GROUP BY e.name
 HAVING count(distinct r.player_id) >= 2;
+
+-- 3)
+SELECT DISTINCT player.name AS player_name, results.event_id
+FROM players player
+JOIN results ON player.player_id = results.player_id
+WHERE results.medal IN ('GOLD', 'SILVER', 'BRONZE');
+
+-- 4)
+SELECT country_id,
+       COUNT(*) as total_players,
+       SUM(CASE WHEN LOWER(SUBSTRING(name, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') THEN 1 ELSE 0 END) as vowel_starting_players
+FROM players
+GROUP BY country_id
+ORDER BY (vowel_starting_players * 100.0) / total_players DESC
+LIMIT 1;
+
+-- 5)
+SELECT countries,
+       COUNT(results.medal) as total_medals,
+       CAST(COUNT(results.medal) as float) / countries.population as medals_per_capita
+FROM results
+JOIN players ON results.player_id = players.player_id
+JOIN countries ON players.country_id = countries.country_id
+JOIN "7thHW".olympics o on countries.country_id = o.country_id
+JOIN "7thHW".events e on e.event_id = results.event_id
+WHERE o.year = 2000
+AND e.is_team_event = 1
+GROUP BY countries, countries.population
+ORDER BY medals_per_capita
+LIMIT 5;
